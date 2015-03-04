5b4f17ecb9a76f0c57254bdc4fab5ca5bb17fe12
KFlamsted <flamsted@gmail.com>
2015-03-04 21:50:29 +0100

    lot of random stuff

--- Eventzer/Global.asax.cs
+++ Eventzer/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Eventzer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,10 +8,24 @@ using System.Web.Routing;
 
 namespace Eventzer
 {
+
     public class WebApiApplication : System.Web.HttpApplication
     {
+        List<Event> events = new List<Event>();
+        bool first_start = false;
+        
         protected void Application_Start()
         {
+            //loading in some test data on application startup
+            if(!first_start)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    events.Add(new Event(i));    
+                }
+                Application["Events"] = events;
+                first_start = true;
+            }
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
