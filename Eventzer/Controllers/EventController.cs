d6a3bb5da4e60908cb922ce08f56e421b0e235ac
KFlamsted <flamsted@gmail.com>
2015-03-04 17:27:42 +0100

    Controllers added, but only the eventcontroller is doing anything atm

---
+++ Eventzer/Controllers/EventController.cs
@@ -0,0 +1,38 @@
+﻿using Eventzer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Eventzer.Controllers
+{
+    public class EventController : ApiController
+    {
+        List<Event> events = new List<Event>
+        {
+            new Event(1),
+            new Event(2),
+            new Event(3)
+        };
+
+        //returning all events created
+        public IEnumerable<Event> GetAllEvents()
+        {
+            return events;
+        }
+
+        //finding the event with a specific id
+        public IHttpActionResult GetEvent(int id)
+        {
+            var one_event = events.FirstOrDefault((p) => p.id == id);
+            if (one_event == null)
+            {
+                return NotFound();
+            }
+            return Ok(one_event);
+        }
+   
+    }
+}
